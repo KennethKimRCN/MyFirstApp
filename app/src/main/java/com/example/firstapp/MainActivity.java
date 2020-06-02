c6b03a43ba29ebe7668cb92e8fe3c446903e7388
KennethKimRCN <rkdsus93@gmail.com>
2020-06-03 03:25:42 +0900

    added functioning app

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //To reference it, create an object of the reference type
+        Button addBtn = (Button) findViewById(R.id.addBtn); //R is your resource object
+
+        //Next we are going to assign an on-click listener
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This casts into a EditText object
+                EditText firstNumEditText = (EditText) findViewById(R.id.input1);
+                EditText secondNumEditText = (EditText) findViewById(R.id.input2);
+
+                TextView resultTextView = (TextView) findViewById(R.id.resultNum);
+
+                //This changes the string to an integer
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTextView.setText(result + "");
+            }
+        });
     }
 }
